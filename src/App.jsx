177154c3c060051ed8e77bdc0c8d2ffed3dab8b9
Anjaliujjainia145 <152694177+Anjaliujjainia145@users.noreply.github.com>
2024-06-19 12:45:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Learn about redux Toolkit</h1>
+    <AddTodo/>
+    <Todo/>
+    </>
+  )
+}
+
+export default App
